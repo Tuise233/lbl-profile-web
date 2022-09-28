396dc6e85df4094f5338bc927ceb20ee8de57b5a
Tuise233 <2039203690@qq.com>
2022-09-29 02:43:23 +0800

    兼容手机客户端

--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="app">
-    <div class="app-container">
-        <div class="app-tab">
+  <div :class="isMobile == false ? 'app' : 'app-pe'">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
+    <meta name="viewport" content="viewport-fit=cover" />
+    <div :class="isMobile == false ? 'app-container' : 'app-container-pe'">
+        <div class="app-tab" v-if="isMobile == false">
             <div class="app-tab-header">
                 <span>我的经历</span>
             </div>
@@ -32,24 +34,29 @@
             </div>
         </div>
 
-        <div class="app-self">
+        <div :class="isMobile == false ? 'app-self' : 'app-self-pe'">
             <img src="@/assets/img/logo.png">
-            <span style="color: white; font-size: 25px; font-weight: bold; margin-top: 20px;">拉布拉马</span>
-            <span style="color: white; font-size: 15px; margin-top: 10px;">业余编程狂热爱好者</span>
-            <span style="color: white; font-size: 15px; margin-top: 30px;">欢迎访问我的个人主页</span>
-            <div class="app-self-tab">
-                <div class="app-self-tab-item"
+            <span :style="isMobile == false ? {color: 'white', fontSize: '25px', fontWeight: 'bold', marginTop: '20px'} : 
+            {color: 'white', fontSize: '50px', fontWeight: 'bold', marginTop: '20px'}">拉布拉马</span>
+            <span :style="isMobile == false ? {color: 'white', fontSize: '15px', marginTop: '10px'} : 
+            {color: 'white', fontSize: '40px', marginTop: '10px'}">业余编程狂热爱好者</span>
+            <span :style="isMobile == false ? {color: 'white', fontSize: '15px', marginTop: '30px'} : 
+            {color: 'white', fontSize: '40px', marginTop: '30px'}">欢迎访问我的个人主页</span>
+            <div :class="isMobile == false ? 'app-self-tab' : 'app-self-tab-pe'">
+                <div :class="isMobile == false ? 'app-self-tab-item' : 'app-self-tab-item-pe'"
                     @click="enterUrl('https://space.bilibili.com/8013472?spm_id_from=333.1007.0.0')">
                     <img src="@/assets/img/bilibili.png">
                     <span>哔哩哔哩</span>
                 </div>
 
-                <div class="app-self-tab-item"
+                <div :class="isMobile == false ? 'app-self-tab-item' : 'app-self-tab-item-pe'"
                     @click="enterUrl('https://github.com/Tuise233/')">
                     <img src="@/assets/img/github.png">
                     <span>Github</span>
                 </div>
             </div>
+            <span v-if="isMobile == true" style="margin-top: 100px; font-size: 30px; color: white;">完整信息请访问电脑版个人主页</span>
+            <span v-if="isMobile == true" style="margin-top: auto; margin-bottom: 40px; font-size: 30px; color: white;">© 2022 拉布拉马</span>
         </div>
     </div>
   </div>
@@ -94,8 +101,14 @@ export default {
             }, {
                 name: "小游戏",
                 list: []
-            }]
+            }],
+
+            isMobile: false
         }
+    },
+
+    created() {
+        this.isMobile = this._isMobile() == null ? false : true;
     },
 
     methods: {
@@ -103,12 +116,18 @@ export default {
             if(url != "" && url != undefined){
                 window.open(url, "_blank");
             }
+        },
+
+        _isMobile() {
+            let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+            return flag;
         }
     },
 }
 </script>
 
 <style>
+/* Pc Css */
 .app{
     width: 100vw;
     height: 100vh;
@@ -265,6 +284,74 @@ export default {
 .app-self-tab-item img{
     width: 20px;
     height: 20px;
+    margin-top: 0;
+    margin-right: 5px;
+}
+
+/* Pe Css */
+.app-pe{
+    width: 100vw;
+    height: 100vh;
+    background-image: url("@/assets/img/bg.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+.app-container-pe{
+    width: 100vw;
+    height: 100vh;
+    backdrop-filter: blur(10px);
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: flex-end;
+}
+
+.app-self-pe{
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.app-self-pe img{
+    width: 350px;
+    height: 350px;
+
+    margin-top: 300px;
+}
+
+.app-self-tab-pe{
+    display: flex;
+    align-items: center;
+    margin-top: 30px;
+}
+
+.app-self-tab-item-pe{
+    width: 250px;
+    height: 120px;
+    background-color: rgba(255, 255, 255, 0.1);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px 40px;
+    transition: 0.3s all;
+    border-radius: 20px;
+}
+
+.app-self-tab-item-pe:hover{
+    background-color: rgba(255, 255, 255, 0.3);
+}
+
+.app-self-tab-item-pe span{
+    color: white;
+    font-size: 35px;
+}
+
+.app-self-tab-item-pe img{
+    width: 50px;
+    height: 50px;
     margin-top: 0;
     margin-right: 5px;
 }
